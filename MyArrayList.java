1274cb04ddfa9381e0db8c4a9d57b283e5d9a4c3
anuhridyas <anuhridya.s@gmail.com>
2016-03-27 18:36:50 -0400

    Update MyArrayList.java

--- MyArrayList.java
+++ MyArrayList.java
@@ -1,5 +1,3 @@
-/* package codechef; // don't place package name! */
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
@@ -25,7 +23,7 @@ class MyArrayList<E> //throws Exception
     
     void add (E e)
     {
-    if (size==default_size)
+    if (size==default_size-1)
     {
         resize();
     }
@@ -50,16 +48,20 @@ class MyArrayList<E> //throws Exception
     
     void remove( int i )
     {
-        if(i<0 || i >=default_size){
+       
+        if(i<0 || i >size)
+        {
         throw new IndexOutOfBoundsException();
         }
-        for(int startingPoint=i;startingPoint<default_size;startingPoint++)
+        
+        for(int startingPoint=i;startingPoint<size;startingPoint++)
         {
+           
           arr[startingPoint]=arr[startingPoint+1];
           
           
         }
-        //arr[size]=null;
+        arr[size]=null;
         size--;
     }
     
@@ -68,37 +70,98 @@ class MyArrayList<E> //throws Exception
      for(int i=0;i<size;i++) {
          arr[i]=null;
      }  
-     size=0;
+     size=-1;
     }
     
     
    E get (int i)
    {
-       if(i<0 && i>size)
+       if(i<0 || i>size)
        {
           throw new IndexOutOfBoundsException();
        }
       return(arr[i]);   
    }
     
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		MyArrayList<String> arr = new MyArrayList<String>();
-		arr.add("ww");
-		arr.add("yy");
-	    int Y=arr.size();
-	    System.out.println(Y);
-	    for(int i = 0; i < arr.size(); i++) {
-	        System.out.println(arr.get(i));
-	    }
-	    boolean thrown = false;
-
+    void set (int index,E value)
+    {
+        if(index<0 || index >size)
+        {
+         throw new IndexOutOfBoundsException();   
+        }
+        
+        arr[index]=value;
+    }
+    
+    public static boolean testArrayListAdd() {
+        MyArrayList<String> arr = new MyArrayList<String>();
+		arr.add("my");
+	    return arr.size() == 1;
+    }
+    
+    public static boolean testArrayListremove() {
+        MyArrayList<String> arr = new MyArrayList<String>();
+        arr.add("my");
+        arr.remove(0);
+        return arr.size() == 0;
+    }
+    
+    
+    
+    public static boolean testArrayListGet() {
+        MyArrayList<String> arr = new MyArrayList<String>();
+        arr.add("my");
+        return arr.get(0) == "my";
+    }
+    
+    
+    public static boolean testArrayListSet() {
+        MyArrayList<String> arr = new MyArrayList<String>();
+        arr.add("my");
+        arr.set(0,"anu");
+        return arr.get(0).equals("anu");
+    }
+    
+    public static boolean testArrayListResize() {
+        MyArrayList<String> arr = new MyArrayList<String>();
+        arr.add("my");
+        arr.set(0,"anu");
+        
+        for(int i = 0; i < 20; ++i) {
+            arr.add(Integer.toString(i));
+        }
+        return arr.size()==21;
+    }
+  
+  
+   public static boolean testArrayListException() {
+        boolean thrown = false;
+    
+        MyArrayList<String> arr = new MyArrayList<String>();
+    	arr.add("my");
+        arr.set(0,"anu");
+        
         try {
-            System.out.println(arr.get(4));
+            for(int i = 0; i < 20; ++i) {
+                arr.remove((i));
+            }
+            for(int i = 0; i < arr.size(); ++i) {
+                System.out.print(arr.get(i) + ", ");
+            }
         } catch (IndexOutOfBoundsException e) {
             thrown = true;
         }
+        
+        return thrown;
+    }
     
-        System.out.println(thrown);
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        System.out.println("testArrayListAdd:" + testArrayListAdd());
+        System.out.println("testArrayListremove:" + testArrayListremove());
+        System.out.println("testArrayListget:" + testArrayListGet());
+        System.out.println("testArrayListset:" + testArrayListSet());
+        System.out.println("testArrayListResize:" + testArrayListResize() );
+        System.out.println("testArrayListException:" + testArrayListException() );
     }
 }
