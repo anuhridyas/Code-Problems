7f73980d0dfdc4927d214016f02ff50938b66894
anuhridyas <anuhridya.s@gmail.com>
2016-03-29 16:46:37 -0400

    All tree traversal iterative and recursive

--- PreorderIterative.java
+++ PreorderIterative.java
@@ -64,17 +64,91 @@ public class PreorderIterative {
             }
         }
     }
+    public static  void inOrderIterative(Node root){
+        if(root==null){
+            return;
+        }
+        Stack<Node> s=new Stack<Node>();
+        Node current=root;
+        while(!s.isEmpty()|| current!=null){
+            while(current!=null){
+                s.push(current);
+                current=current.left;
+            }
+            Node n=s.pop();
+            System.out.println(n.value);
+            current=n.right;
+        }
 
+    }
+
+
+    public static void inOrder(Node root){
+        if(root==null){
+            return ;
+        }
+        inOrder(root.left);
+        System.out.println(root.value);
+        inOrder(root.right);
+    }
+
+    public static void postOrder(Node root)
+    {
+        if(root==null){
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.println(root.value);
+    }
+
+
+    public  static  void postOrderIterative(Node root){
+        if(root==null){
+            return;
+        }
+        Stack<Node> s1=new Stack<>();
+        Stack<Node> s2=new Stack<>();
+        s1.push(root);
+        while (!s1.isEmpty()){
+            Node current= s1.pop();
+            s2.push(current);
+            if(current.left!=null){
+                s1.push(current.left);
+            }
+            if(current.right!=null){
+                s1.push(current.right);
+            }
+        }
+        while(!s2.isEmpty()){
+            System.out.println(s2.pop().value);
+        }
+    }
     public static void main (String[] args){
         root=insert(root,8);
         root=insert(root,1);
         root=insert(root,10);
         root=insert(root,4);
+        root=insert(root,9);
+        root=insert(root,2);
+        root=insert(root,40);
+        root=insert(root,30);
+        root=insert(root,34);
+        root=insert(root,23);
+        root=insert(root,25);
+        root=insert(root,87);
         System.out.println("Recursive preorder traversal of the tree");
         preOrder(root);
         System.out.println("iterative preorder traversal of the tree");
         preOrderIterative(root);
-
+        System.out.println("iterative INORDER traversal of the tree");
+        inOrderIterative(root);
+        System.out.println("Recursive inorder traversal of the tree");
+        inOrder(root);
+        System.out.println("Recursive postorder traversal of the tree");
+        postOrder(root);
+        System.out.println("iterative postorder traversal of the tree");
+        postOrderIterative(root);
 
 
     }
